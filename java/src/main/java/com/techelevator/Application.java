36dfc30cc1ba042b46fa022ac7859ee99a0172c6
andrewl <andrewl@udel.edu>
2025-06-24 13:32:29 -0400

    restarting app

--- java/src/main/java/com/techelevator/Application.java
+++ java/src/main/java/com/techelevator/Application.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    
+
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
-        DanceSearchApp danceSearchApp = new DanceSearchApp();
-        danceSearchApp.Run();
+        SpringApplication.run(Application.class, args);
     }
 
 }
